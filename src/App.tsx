c8e0a995c2138ad11cf5dd3a063481fa01ac1bb9
Michael McNair <michaeltsuyoshi@gmail.com>
2022-11-15 13:50:28 +0900

    allow input

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import wordsData from './words.json';
 
 function App() {
-  const [answer, setAnswer] = useState();
-
   function generateGuesses() {
     const guesses: any = [];
     for (let i = 0; i <= 5; i++) {
       guesses.push([]);
       for (let j = 0; j <= 4; j++) {
-        guesses[i].push({ char: null, included: false, correct: false });
+        guesses[i].push({ char: '', included: false, correct: false });
       }
     }
     return guesses;
   }
 
+  const [answer, setAnswer] = useState();
   const [guesses, setGuesses] = useState(generateGuesses());
 
   let words: any;
 
   useEffect(() => {
-    words = wordsData.map((word) => word.word.toLowerCase());
+    words = wordsData.map((wordObj) => wordObj.word.toLowerCase());
     setAnswer(words[Math.floor(Math.random() * words.length)]);
   }, []);
 
@@ -37,12 +36,39 @@ function App() {
     }
   }, [guesses]);
 
+  function handleType(e: any, rowIdx: number, boxIdx: number) {
+    setGuesses((prevGuesses: any) =>
+      prevGuesses.map((row: any, prevRowIdx: number) =>
+        row.map((box: any, prevBoxIdx: number) => {
+          if (rowIdx === prevRowIdx && boxIdx === prevBoxIdx) {
+            return { ...box, char: e.target.value };
+          }
+          if (rowIdx === prevRowIdx + 1 && boxIdx === prevBoxIdx) {
+            // focus on next box
+          }
+          return box;
+        })
+      )
+    );
+    console.dir(e.target);
+  }
+
   return (
     <div className="App">
       <div className="grid">
         {guesses.map((row: any, rowIdx: number) =>
           row.map((box: any, boxIdx: number) => {
-            return <input type="text" key={rowIdx + boxIdx} className="box" />;
+            return (
+              <input
+                type="text"
+                key={rowIdx + boxIdx}
+                className="box"
+                value={guesses[rowIdx][boxIdx].char}
+                onChange={(e) => handleType(e, rowIdx, boxIdx)}
+                maxLength={1}
+                autoFocus={rowIdx === 0 && boxIdx === 0 ? true : false}
+              />
+            );
           })
         )}
       </div>
